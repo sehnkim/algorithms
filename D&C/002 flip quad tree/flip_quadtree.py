5cf54fd0121e862d72b165eba698077f902775db
Sehwan Kim <kim@worldviz.com>
2017-02-10 07:26:33 -0500

    divisible max subset

---
+++ D&C/002 flip quad tree/flip_quadtree.py
@@ -0,0 +1,22 @@
+﻿def find(d, y, x, size):
+	if len(d) == 1:
+		return d
+
+	str = ['','','','', '']
+	if d[0] == 'x':
+		step = 0
+		for i in range(1, 5):
+			if d[i + step] == 'x':
+				str[i] += find(d[(i+step):])
+				step += len(str[i])
+				step -= 1
+			else:
+				str[i] += d[i + step]
+		return d[0] + str[3] + str[4] + str[1] + str[2]
+
+
+#print find('xbwxwbbwb')
+print find('xxwwwbxwxbbbwwxxxwwbbbwwwwbb')
+
+
+
---
+++ recursive_DP/004_divisible_max_subset/divisible_max_subset.py
@@ -0,0 +1,44 @@
+﻿# input: [1,2,4,8]
+
+import itertools
+
+# input: [1,2,4,8]
+
+arr = [1,2,4,8]
+
+def f(arr, answer):
+    if len(arr) == 2:
+        if arr[0] % arr[1] == 0 or arr[1] % arr[0] == 0:
+            if len(arr) > len(answer):
+                answer = arr
+            return True
+        else:
+            return False
+
+    # 1: divisible?
+    if (len(arr) > 2):
+        subsets = itertools.combinations(arr, len(arr)-1)
+        res = [f(s, answer) for s in subsets]
+        r = reduce(lambda x, y: x*y, res)
+        if r and len(arr) > len(answer):
+            answer = arr
+        return r
+
+
+answer = []
+f(arr, answer)
+print answer
+
+
+#         F
+#      1,2,3,4
+#
+#     T        F
+#   1,2,4    1,2,3
+#
+#  (T   T)  (T   F)
+# 1,2  2,4  1,3  2,3
+#
+
+
+
